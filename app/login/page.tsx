4469eed246527602f325d6ef68102787ef37c9a4
higord3v <higord3v@gmail.com>
2025-08-26 13:17:27 -0300

    refactor: ajustado css

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -83,7 +83,7 @@ export default function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="max-w-md w-full space-y-8 p-8 bg-white rounded-lg shadow">
+      <div className="max-w-md w-full space-y-8 p-20 bg-white rounded-lg shadow">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             PSL LEGAL - {isSignUp ? 'Cadastro' : 'Login'}
