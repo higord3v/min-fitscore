ac1cf23eada205ed1f498a24604522c070a269cb
higord3v <higord3v@gmail.com>
2025-08-27 19:04:42 -0300

    refactor: eslint e outros

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,10 +24,7 @@ const eslintConfig = [
     // Configurações personalizadas para desabilitar regras específicas
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", { 
-        "argsIgnorePattern": "^_",
-        "varsIgnorePattern": "^_" 
-      }],
+      "@typescript-eslint/no-unused-vars": "off",
       "prefer-const": "warn",
       "react-hooks/exhaustive-deps": "warn"
     }
